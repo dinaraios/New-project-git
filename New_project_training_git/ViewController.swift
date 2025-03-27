c61358d4495edc9fea0d6516dec8eb97bbf54e8d
dinaraios <dinara87@yahoo.com>
2025-03-27 09:56:34 +0500

    first commit

---
+++ New_project_training_git/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  New_project_training_git
+//
+//  Created by Dinara Ishanova on 27.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+fork надо открыть}
+
